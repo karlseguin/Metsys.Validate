396e20591a301de5f8e5c630066ced75f9b31080
Karl Seguin <kseguin@epocal.com>
2010-04-08 16:51:13 -0400

    server side validation for CannedPatterns (except CreditCard) implemented

--- Metsys.Validate.Tests/CannedPatternValidatorTests.cs
+++ Metsys.Validate.Tests/CannedPatternValidatorTests.cs
@@ -31,5 +31,81 @@ namespace Metsys.Validate.Tests
         {
             Assert.Equal(1, new CannedPatternValidator(ValidationPattern.Digits).ToJson().Count(kvp => kvp.Key == "digits" && kvp.Value == "true"));
         }
+
+        [Fact]
+        public void InvalidEmailsDoNotValidate()
+        {
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Email).IsValid("I'm cool"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Email).IsValid("@about.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Email).IsValid("me@me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Email).IsValid("me@.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid(new object()));
+        }
+        [Fact]
+        public void ValidOrEmptyEmailsValidate()
+        {
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Email).IsValid(string.Empty));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Email).IsValid("prepare@to.laugh"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Email).IsValid("goku.ownz@your.ass"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Email).IsValid("and@you.likeitalot"));            
+        }
+
+        [Fact]
+        public void InvalidUrlDoNotValidate()
+        {
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid("I'm cool"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid("@about.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid("me@me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid("me@.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Url).IsValid(new object()));
+        }
+        [Fact]
+        public void ValidOrEmptyUrlsValidate()
+        {
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Url).IsValid(string.Empty));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Url).IsValid("http://goku.owns"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Url).IsValid("http://www.goku.owns"));            
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Url).IsValid("http://goku.owns/ur?ass=true"));            
+        }
+
+        [Fact]
+        public void InvalidNumberDoNotValidate()
+        {
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid("I'm cool"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid("@about.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid("me@me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid("me@.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid("43.34.23"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Number).IsValid(new object()));
+        }
+        [Fact]
+        public void ValidOrEmptyNumberValidate()
+        {
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid(string.Empty));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid("0"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid("23"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid("-23"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid("-23.4"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Number).IsValid("43.343"));
+        }
+
+        [Fact]
+        public void InvalidDigitsDoNotValidate()
+        {
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("I'm cool"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("@about.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("me@me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("me@.me"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("43.34.23"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid("43.34"));
+            Assert.Equal(false, new CannedPatternValidator(ValidationPattern.Digits).IsValid(new object()));
+        }
+        [Fact]
+        public void ValidOrEmptyDigitsValidate()
+        {
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Digits).IsValid(string.Empty));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Digits).IsValid("0"));
+            Assert.Equal(true, new CannedPatternValidator(ValidationPattern.Digits).IsValid("23"));            
+        }
     }
 }
--- Metsys.Validate/Validators/CannedPatternValidator.cs
+++ Metsys.Validate/Validators/CannedPatternValidator.cs
@@ -1,17 +1,27 @@
-using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+//email and url regular expressions are by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
 
 namespace Metsys.Validate.Validators
-{
+{ 
     internal sealed class CannedPatternValidator : IValidator, IDoJavascript
     {
         private static readonly IDictionary<ValidationPattern, KeyValuePair<string, string>> _jsonLookup = new Dictionary<ValidationPattern, KeyValuePair<string, string>>
                                                                                 {
                                                                                     {ValidationPattern.Email, new KeyValuePair<string, string>("email", "true")},
                                                                                     {ValidationPattern.Digits, new KeyValuePair<string, string>("digits", "true")},
-                                                                                    {ValidationPattern.Number, new KeyValuePair<string, string>("number", "true")},                                                                                    
+                                                                                    {ValidationPattern.Number, new KeyValuePair<string, string>("number", "true")},
                                                                                     {ValidationPattern.CreditCard, new KeyValuePair<string, string>("creditcard", "true")},
                                                                                     {ValidationPattern.Url, new KeyValuePair<string, string>("url", "true")},
+                                                                                };
+
+        private static readonly IDictionary<ValidationPattern, Regex> _regexLookup = new Dictionary<ValidationPattern, Regex>
+                                                                                {
+                                                                                    {ValidationPattern.Email, new Regex(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", RegexOptions.Compiled | RegexOptions.IgnoreCase)},
+                                                                                    {ValidationPattern.Digits, new Regex(@"^\d+$", RegexOptions.Compiled | RegexOptions.IgnoreCase)},
+                                                                                    {ValidationPattern.Number, new Regex(@"^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$", RegexOptions.Compiled | RegexOptions.IgnoreCase)},
+                                                                                    {ValidationPattern.Url, new Regex(@"^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$", RegexOptions.Compiled | RegexOptions.IgnoreCase)},
                                                                                 };
     
         private readonly ValidationPattern _pattern;
@@ -20,14 +30,25 @@ namespace Metsys.Validate.Validators
         {
             _pattern = pattern;
         }
-        public bool IsValid(object value)
-        {
-            throw new NotImplementedException();
-        }
 
         public IEnumerable<KeyValuePair<string, string>> ToJson()
         {
             yield return _jsonLookup[_pattern];
         }
+        
+        public bool IsValid(object value)
+        {
+            if (value == null) { return true; }
+            var @string = value.ToString();
+            if (@string.Length == 0) { return true; }
+            return _pattern == ValidationPattern.CreditCard ? IsValidCreditCard(@string) : _regexLookup[_pattern].IsMatch(@string);
+        }
+
+        private static bool IsValidCreditCard(string @string)
+        {
+            return true; //todo 
+        }
+
+
     }
 }
